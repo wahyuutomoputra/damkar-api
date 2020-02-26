360af1be5eeaeaea010c648da2fe9e9f7dc1f44f
wahyuutomoputra@gmail.com <wahyuutomoputra@gmail.com>
2020-02-26 17:06:04 +0700

    upload

--- controllers/laporanController.js
+++ controllers/laporanController.js
@@ -3,15 +3,13 @@ const Laporan = require("../models/laporanModel");
 
 exports.addLaporan = (req, res, next) => {
     let sampleFile = req.files.avatar;
+    let nameImage = sampleFile.name.split(' '). join('');
 
-    sampleFile.mv('./uploads/filname2.jpg', function(err){
+    sampleFile.mv('./uploads/'+nameImage, function(err){
         if(err){
             return res.status(500).send(err);
         }else{
             res.send('File uploaded!');
         }
     })
-
-    console.log(sampleFile)
-
 }
